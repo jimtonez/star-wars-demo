3c70c078ab52740e0af40de4b50b7e4e03f0e4d5
Tim Jones <timjones@redhat.com>
2023-04-20 09:47:14 -0600

    removing aspect-ratio css props

--- components/ShipDisplay.tsx
+++ components/ShipDisplay.tsx
@@ -14,7 +14,7 @@ const ShipDisplay = ( char: Display ) => {
     const [color, setColor] = useRecoilState(colorState)
 
     return (
-        <div className={`col-span-1 aspect-w-2 aspect-h-2 cursor-pointer shadow-b shadow-sm ease-in-out duration-300`}>
+        <div className={`col-span-1 cursor-pointer shadow-b shadow-sm ease-in-out duration-300`}>
             <div className="flex relative flex-col items-center justify-center">
                 <div className='flex h-48 w-48 items-center justify-center mt-4 border border-gray-700 rounded-full shadow-lg shadow-black'>
                     <Image priority alt="" className={`h-[100%] w-[100%] rounded-full`} src={char.image} width={500} height={500} />
