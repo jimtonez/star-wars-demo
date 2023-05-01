62ff16f97a970296418f50ba02eec69cfa125954
Tim Jones <timjones@redhat.com>
2023-04-30 22:16:39 -0600

    debugging

--- pythonDjango/pythonDjango/settings/development.py
+++ pythonDjango/pythonDjango/settings/development.py
@@ -70,7 +70,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# All settings common to all environments
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
