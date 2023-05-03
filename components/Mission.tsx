402b1e5cbd799857f2281abe6f71e6b5052dd1ee
Tim Jones <timjones@redhat.com>
2023-05-03 09:46:33 -0600

    fixing mission links

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from 'next/navigation'
 import useHover from "@/hooks/useHover"
 import { ArrowRightCircleIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 import { profile } from "console";
 
 interface Mission {
     name: string,
+    index: number,
     image: string,
     enabled: boolean
 }
@@ -14,11 +15,20 @@ export default function Mission ( mission: Mission) {
 
     const [hoverRef, isHovered] = useHover(false);
     const missionName = mission.name.toLowerCase()
+    const router = useRouter()
+
+    const handleClick = (name: string) => {
+        if (mission.index > 0){
+            return
+        } else {
+            router.push(`/missions/${missionName}`)
+        }
+    }
 
     return (
         <div className="flex flex-col items-center justify-center">
             <p className='md:hidden text-white text-2xl sm:text-3xl md:text-2xl lg:text-4xl font-mono'>{mission.name}</p>
-            <Link href={`/missions/${missionName}`} ref={hoverRef as React.RefObject<HTMLDivElement>} className={`flex ${mission.enabled ? 'cursor-pointer' : ''} flex-col col-span-1 h-full w-auto lg:w-full border ${!isHovered ? 'border-yellow-600 bg-inherit' : 'border-gray-700 bg-gray-800/20 scale-[101%]'} rounded-xl shadow-lg shadow-black`}>
+            <button onClick={() => handleClick(missionName)} ref={hoverRef as React.RefObject<HTMLDivElement>} className={`flex ${mission.enabled ? 'cursor-pointer' : ''} flex-col col-span-1 h-full w-auto lg:w-full border ${!isHovered ? 'border-yellow-600 bg-inherit' : 'border-gray-700 bg-gray-800/20 scale-[101%]'} rounded-xl shadow-lg shadow-black`}>
                 <div className={`hidden md:flex w-full h-14 lg:h-20 items-center justify-center rounded-t-xl border-b ${!isHovered ? 'border-yellow-600 bg-zinc-900' : 'bg-zinc-900 border-gray-700'} ease-in-out duration-300`}>
                     <p className='text-white text-2xl sm:text-3xl md:text-2xl lg:text-4xl font-mono'>{mission.name}</p>
                     <div className={`${isHovered ? 'flex' : 'hidden'} absolute items-center justify-center right-4 h-10 w-10 lg:h-12 lg:w-12 rounded-2xl lg:border-2 ${mission.enabled ? 'border-green-400 text-green-400' : 'border-red-600 text-red-600'} hover:scale-[101%] ease-in-out duration-300`}>
@@ -32,7 +42,7 @@ export default function Mission ( mission: Mission) {
                 <div className="flex w-auto h-auto">
                     <Image className="rounded-xl md:rounded-none md:rounded-b-xl object-cover opacity-60" alt="" src={mission.image} width={500} height={400} />
                 </div>
-            </Link>
+            </button>
         </div>
     )
 }
